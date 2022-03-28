222128891983fe370d1210c04df580f40d63d794
Ardhan_Luqman <81498689+Boncabelvl30@users.noreply.github.com>
2022-03-28 19:59:30 +0700

    Initial commit

---
+++ lib/main_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatelessWidget {
+  const MainScreen ({Key? key}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Wisata Surabaya'),
+      ),
+      body: listItem(),
+    );
+  }
+  Widget listItem(){
+    return Card();
+  }
+}
