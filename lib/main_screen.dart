479cddf39df2c40ca581d4689a09c17adebc0817
HudzaiLabib <hudzaifahallabib@gmail.com>
2022-03-29 13:02:29 +0700

    second commit

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatelessWidget {
-  const MainScreen ({Key? key}) : super (key: key);
+  const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +12,38 @@ class MainScreen extends StatelessWidget {
       body: listItem(),
     );
   }
-  Widget listItem(){
-    return Card();
+
+  Widget listItem() {
+    return Card(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Image.asset('assets/images/submarine.jpg'),
+          ),
+          Expanded(
+            flex: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: const [
+                  Text(
+                    'Monumen Kapal Selam',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text('Surabaya'),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
